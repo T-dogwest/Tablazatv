4e3cd58927e3f202aca55215c9951cf8e7ba2f81
Csontos <csontostibor48@gmail.com>
2024-01-31 11:21:24 +0100

    try

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma.service';
@@ -7,32 +7,57 @@ import { PrismaService } from 'src/prisma.service';
 export class UserService {
   constructor(readonly db:PrismaService){}
 
-  create(createUserDto: CreateUserDto) {
-    return this.db.user.create({
-      data :createUserDto
-    });
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const newUser = await this.db.user.create({
+        data: createUserDto,
+      });
+      return newUser; 
+    } catch (error) {
+      throw new NotFoundException('nem lehet létrehozni a felhasználót'); 
+    }
   }
 
-  findAll() {
-    return this.db.user.findMany();
+  async findAll() {
+    try {
+      const users = await this.db.user.findMany();
+      return users; 
+    } catch (error) {
+      throw new NotFoundException('nem talákható felhasználó'); 
+    }
   }
 
-  findOne(id: number) {
-    return  this.db.user.findUnique({
-      where: { id },
-    });;
+  async findOne(id: number) {
+    try {
+      const user = await this.db.user.findUnique({
+        where: { id },
+      });
+      return user ? `a keresett user ${user.name} ` : 'User not found';
+    } catch (error) {
+      throw new NotFoundException(`Nem található user id ${id}`); 
+      
+    }
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return this.db.user.update({
-      where: { id },
-      data: updateUserDto,
-    });;
+ async update(id: number, updateUserDto: UpdateUserDto) {
+    try {
+      await this.db.user.update({
+        where: { id },
+        data: updateUserDto,
+      });
+      return `a ${id} felhasználó frissítve`; 
+    } catch (error) {
+      throw new NotFoundException(`nem lehet frissíteni a ${id}`); 
+    }
   }
 
-  remove(id: number) {
-    return this.db.user.delete({
-      where: { id },
-    });
-  }
+ async remove(id: number) {
+    try {
+      await this.db.user.delete({
+        where: { id },
+      });
+      return `a ${id} felhasználó törölve`; 
+    } catch (error) {
+      throw new NotFoundException(`nem lehet törölni a ${id} felhasználót`); 
+    }}
 }
